fce62eb1dbe8aba03cb61b0510e3d61e92cdb8b5
Paola <drincon28@uan.edu.co>
2018-10-20 12:18:49 -0500

    change to heroku get started

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
     
     public static void main(String[] args) {
