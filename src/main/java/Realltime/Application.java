68ce1d99ddeb9dca67c6fcf5060563dd74c24ca2
Paola <drincon28@uan.edu.co>
2018-11-10 11:35:28 -0500

    Adicion de reportes

--- src/main/java/Realltime/Application.java
+++ src/main/java/Realltime/Application.java
@@ -1,9 +1,8 @@
-package hello;
+package Realltime;
 
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 //@SpringBootApplication
